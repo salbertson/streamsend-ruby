17989a0274c04755a5e463bfce1c986123d27562
Scott Albertson <salbertson@ezpublishing.com>
2011-02-13 21:55:31 -0800

    Use .from_xml for parsing XML and creating a hash.

--- streamsend.rb
+++ streamsend.rb
@@ -1,7 +1,7 @@
 require 'uri'
 require 'net/http'
 require 'net/https'
-require 'rexml/document'
+require 'activesupport'
 
 module StreamSend
   HOST = "app.streamsend.com"
@@ -27,26 +27,6 @@ module StreamSend
     http.request(request).body
   end
 
-  def self.xml_to_attribute_hashes(xml, fields)
-    collection = []
-    doc = REXML::Document.new(xml)
-    doc.elements.first.elements.each do |element|
-      data = {}
-      fields.each do |field_name|
-        field = element.elements[field_name]
-        data[field_name.gsub(/-/, "_")] = case field.attribute("type").to_s
-        when "integer"
-          field.text.to_i
-        else
-          field.text
-        end
-      end
-      collection << data
-    end
-
-    collection
-  end
-
   class Resource
     def initialize(data)
       @data = data
@@ -68,12 +48,7 @@ module StreamSend
   class Subscriber < Resource
     def self.all(audience_id = 1)
       xml = StreamSend.get("/audiences/#{audience_id}/people.xml")
-      subscriber_data = StreamSend.xml_to_attribute_hashes(xml, %w[id email-address opt-status tracking-hash created-at])
-      subscribers = []
-      subscriber_data.each do |data|
-        subscribers << new(data)
-      end
-      subscribers
+      Hash.from_xml(xml)["people"].collect { |data| new(data) }
     end
   end
 end
